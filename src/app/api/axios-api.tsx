dec5637599685b6f98ad22695e12ba5912c9eb4f
joshoti <joshoti539@gmail.com>
2024-11-15 17:39:18 -0800

    feat: Axios for communicating with python backend

---
+++ src/app/api/axios-api.tsx
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+/* Would otherwise use dotenv or vite to import environment 
+variables eg. import.meta.env.VITE_BASE_URL */
+
+export const api = axios.create({
+  baseURL: `http://127.0.0.1:5000`, // Python backend default port
+  headers: {
+    "Content-Type": "application/json",
+  },
+  withCredentials: true,
+});
