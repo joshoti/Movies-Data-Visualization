c623598f76f41e267000af256be7943c4e4f4dea
joshoti <joshoti539@gmail.com>
2024-11-18 00:09:26 -0800

    Added carousel and charts to analysis page

--- src/app/components/Data/Analysis.tsx
+++ src/app/components/Data/Analysis.tsx
@@ -1,54 +1,21 @@
-import { Text, Flex, Title, Table, Space, Box } from "@mantine/core";
+import { Text, Flex, Title, Space } from "@mantine/core";
 import classes from "./Data.module.css";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { docsScrollOptions } from "../../pages/LandingPage";
 import { scrollToElement } from "../../utils/scoll";
+import OverviewGenres from "../DataCharts/GenreOverview";
+import RatingByRuntime from "../DataCharts/RatingByRuntime";
+import { Carousel } from "@mantine/carousel";
+import Autoplay from "embla-carousel-autoplay";
 
+export const marginTop = 40;
 export function Analysis() {
   // To scroll to the top manually
   useEffect(() => {
     scrollToElement("head", docsScrollOptions);
   });
 
-  const errors = [
-    { code: 200, response: "Ok", description: "Login flow was successful" },
-    {
-      code: 400,
-      response: "Bad Request",
-      description:
-        "Typically occurs if a required parameter is missing or malformed",
-    },
-    {
-      code: 401,
-      response: "Unauthorized",
-      description: "Invalid credentials or missing authentication token",
-    },
-    {
-      code: 403,
-      response: "Forbidden",
-      description: "Access denied due to insufficient permissions",
-    },
-    {
-      code: 429,
-      response: "Rate Limit Exceeded",
-      description: "Access denied due to insufficient permissions",
-    },
-    {
-      code: 500,
-      response: "Internal Server Error",
-      description: "A server issue that may require retrying later",
-    },
-  ];
-
-  const errorTableRows = errors.map((error) => (
-    <Table.Tr key={error.code}>
-      <Table.Td>{error.code}</Table.Td>
-      <Table.Td>{error.response}</Table.Td>
-      <Table.Td>{error.description}</Table.Td>
-    </Table.Tr>
-  ));
-
-  const marginTop = 40;
+  const autoplay = useRef(Autoplay({ delay: 4000 }));
 
   return (
     <Flex h={"100%"}>
@@ -56,41 +23,31 @@ export function Analysis() {
       <Flex flex={1} direction="column">
         <Title
           id="head"
-          mt={marginTop}
+          mt={marginTop / 2}
           mb={marginTop / 2}
           className={classes.title}
         >
-          Error Codes
+          Analysis
         </Title>
-        <Text mb={10} className={classes.regularText}>
-          Below are error codes you might encounter and what they mean:
-        </Text>
-        <Box className={classes.table}>
-          <Table withColumnBorders verticalSpacing="sm" horizontalSpacing="md">
-            <Table.Thead>
-              <Table.Tr className={classes.tableHeader}>
-                <Table.Th>Status Code</Table.Th>
-                <Table.Th>Response</Table.Th>
-                <Table.Th>Description</Table.Th>
-              </Table.Tr>
-            </Table.Thead>
-            <Table.Tbody className={classes.tableBody}>
-              {errorTableRows}
-            </Table.Tbody>
-          </Table>
-        </Box>
-        <Space h={40} />
-      </Flex>
 
-      {/* Table of Contents */}
-      <Flex flex={0.4} direction="column" className={classes.tocContainer}>
-        <Text
-          className={classes.tableOfContentTitle}
-          mt={marginTop + 10}
-          mb={marginTop / 2}
+        <Carousel
+          className={classes.table}
+          withIndicators
+          height={500}
+          plugins={[autoplay.current]}
+          onMouseEnter={autoplay.current.stop}
+          // onMouseLeave={autoplay.current.reset}
         >
-          Table of Contents
-        </Text>
+          <Carousel.Slide>
+            <OverviewGenres />
+          </Carousel.Slide>
+          <Carousel.Slide>
+            <RatingByRuntime />
+          </Carousel.Slide>
+          <Carousel.Slide>3</Carousel.Slide>
+          {/* ...other slides */}
+        </Carousel>
+        <Space h={40} />
       </Flex>
     </Flex>
   );
