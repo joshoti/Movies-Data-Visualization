2b819ccf1f7483dc7841199c44cc52d3437394f3
joshoti <joshoti539@gmail.com>
2024-11-18 00:08:50 -0800

    Created rating by runtime chart

---
+++ src/app/components/DataCharts/RatingByRuntime.tsx
@@ -0,0 +1,70 @@
+// import { LineChart, Line, XAxis, YAxis, Tooltip } from "recharts";
+import { Flex, Title, Text } from "@mantine/core";
+import { AreaChart } from "@mantine/charts";
+import { marginTop } from "../Data/Analysis";
+import classes from "../Data/Data.module.css";
+
+const RatingByRuntime = (/*{ data }*/) => {
+  const data = [
+    {
+      rating: 10,
+      runtime: 40,
+    },
+    {
+      rating: 8.5,
+      runtime: 60,
+    },
+    {
+      rating: 8,
+      runtime: 70,
+    },
+    {
+      rating: 9,
+      runtime: 80,
+    },
+    {
+      rating: 6,
+      runtime: 90,
+    },
+  ];
+  const formattedData = data.map((item) => ({
+    runtime: item.runtime,
+    rating: item.rating,
+  }));
+
+  return (
+    // <LineChart width={600} height={200} data={formattedData}>
+    //   <XAxis dataKey="runtime" />
+    //   <YAxis />
+    //   <Tooltip />
+    //   <Line type="monotone" dataKey="rating" stroke="#82ca9d" />
+    // </LineChart>
+
+    <Flex direction={"column"}>
+      <Title
+        id="head"
+        mt={marginTop / 2}
+        mb={marginTop / 2}
+        className={classes.tableOfContentTitle}
+      >
+        Correlation Between Rating and Runtime
+      </Title>
+      <Text mb={10} className={classes.regularText}>
+        Use Case: Check if longer movies tend to have higher or lower ratings
+      </Text>
+      <AreaChart
+        h={400}
+        data={formattedData}
+        curveType="natural"
+        dataKey="runtime"
+        withLegend
+        withPointLabels
+        xAxisLabel="Runtime (Mins)"
+        yAxisLabel="Rating"
+        series={[{ name: "rating", color: "indigo.6" }]}
+      />
+    </Flex>
+  );
+};
+
+export default RatingByRuntime;
