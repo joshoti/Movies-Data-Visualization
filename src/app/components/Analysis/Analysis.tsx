a47324bbc6182fb8667ce6bcc5cad787fcf0a79a
joshoti <joshoti539@gmail.com>
2024-11-22 12:57:19 -0800

    Updated imports in the main Analysis component

--- src/app/components/Analysis/Analysis.tsx
+++ src/app/components/Analysis/Analysis.tsx
@@ -1,15 +1,15 @@
 import Autoplay from "embla-carousel-autoplay";
 import { Carousel } from "@mantine/carousel";
 import { Flex, Title, Space } from "@mantine/core";
-import classes from "./Data.module.css";
+import classes from "./Analysis.module.css";
 import { useEffect, useRef } from "react";
 import { docsScrollOptions } from "../../pages/LandingPage";
 import { scrollToElement } from "../../utils/scoll";
-import GenreByMovieBarChart from "../DataCharts/GenreByMovieBar";
-import RatingByRuntimeAreaChart from "../DataCharts/RatingByRuntimeArea";
-import YearByMoviesBarChart from "../DataCharts/YearlyByMoviesBar";
-import GenreCompositeChart from "../DataCharts/GenreComposite";
-import MoviesByCensorRatingAreaChart from "../DataCharts/MoviesByCensorArea";
+import GenreByMovieBarChart from "../AnalysisCharts/GenreByMovieBar";
+import RatingByRuntimeAreaChart from "../AnalysisCharts/RatingByRuntimeArea";
+import YearByMoviesBarChart from "../AnalysisCharts/YearByMoviesBar";
+import GenreCompositeChart from "../AnalysisCharts/GenreComposite";
+import MoviesByCensorRatingAreaChart from "../AnalysisCharts/MoviesByCensorArea";
 
 export const marginTop = 40;
 export const chartHeight = 500;
