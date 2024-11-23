c860d59d5fd4a415c1e3cb3c1554b31c731e94bd
joshoti <joshoti539@gmail.com>
2024-11-22 23:26:12 -0800

    Updated default data for GenreByMovieBar component

--- src/app/components/data/GenreByMovieData.ts
+++ src/app/components/data/GenreByMovieData.ts
@@ -10,33 +10,73 @@ export type IGenreByMovie = {
   data: Array<genreData>;
 };
 export const genreByMovieData: IGenreByMovie = {
-  min_rating: 0,
   max_rating: 10,
+  min_rating: 0,
   data: [
     {
-      count: 40,
-      rating: 7,
+      count: 1577,
       main_genre: "Action",
+      rating: 6.53,
     },
     {
-      count: 41,
-      rating: 8,
-      main_genre: "Thriller",
+      count: 321,
+      main_genre: "Animation",
+      rating: 7.03,
     },
     {
-      count: 40,
-      rating: 8.5,
+      count: 355,
+      main_genre: "Biography",
+      rating: 7.23,
+    },
+    {
+      count: 296,
+      main_genre: "Adventure",
+      rating: 6.74,
+    },
+    {
+      count: 4,
+      main_genre: "Western",
+      rating: 8.12,
+    },
+    {
+      count: 1027,
       main_genre: "Drama",
+      rating: 7.08,
     },
     {
-      count: 49,
-      rating: 4,
-      main_genre: "Sci-Fi",
+      count: 447,
+      main_genre: "Crime",
+      rating: 7.08,
     },
     {
-      count: 50,
-      rating: 2,
+      count: 1350,
+      main_genre: "Comedy",
+      rating: 6.54,
+    },
+    {
+      count: 142,
       main_genre: "Horror",
+      rating: 6.05,
+    },
+    {
+      count: 26,
+      main_genre: "Mystery",
+      rating: 7.16,
+    },
+    {
+      count: 3,
+      main_genre: "Film-Noir",
+      rating: 7.63,
+    },
+    {
+      count: 13,
+      main_genre: "Fantasy",
+      rating: 6.03,
+    },
+    {
+      count: 1,
+      main_genre: "Musical",
+      rating: 7.0,
     },
   ],
 };
