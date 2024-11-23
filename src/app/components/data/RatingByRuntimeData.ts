f2a2eb998dabfdd2eec54705da0cfcffc26805d1
joshoti <joshoti539@gmail.com>
2024-11-23 01:02:20 -0800

    Created offline data for RatingByRuntime Chart

---
+++ src/app/components/data/RatingByRuntimeData.ts
@@ -0,0 +1,108 @@
+type genreData = {
+  runtime: number;
+  rating: number;
+};
+
+export type IRatingByRuntime = Array<genreData>;
+export const ratingByRuntimeData: IRatingByRuntime = [
+  {
+    rating: 7.8,
+    runtime: 40.0,
+  },
+  {
+    rating: 6.5,
+    runtime: 50.0,
+  },
+  {
+    rating: 7.6,
+    runtime: 60.0,
+  },
+  {
+    rating: 7.0,
+    runtime: 70.0,
+  },
+  {
+    rating: 6.3,
+    runtime: 80.0,
+  },
+  {
+    rating: 6.5,
+    runtime: 90.0,
+  },
+  {
+    rating: 6.6,
+    runtime: 100.0,
+  },
+  {
+    rating: 6.8,
+    runtime: 110.0,
+  },
+  {
+    rating: 7.0,
+    runtime: 120.0,
+  },
+  {
+    rating: 7.2,
+    runtime: 130.0,
+  },
+  {
+    rating: 7.3,
+    runtime: 140.0,
+  },
+  {
+    rating: 7.3,
+    runtime: 150.0,
+  },
+  {
+    rating: 7.5,
+    runtime: 160.0,
+  },
+  {
+    rating: 7.7,
+    runtime: 170.0,
+  },
+  {
+    rating: 7.7,
+    runtime: 180.0,
+  },
+  {
+    rating: 7.7,
+    runtime: 190.0,
+  },
+  {
+    rating: 8.2,
+    runtime: 200.0,
+  },
+  {
+    rating: 7.7,
+    runtime: 210.0,
+  },
+  {
+    rating: 8.1,
+    runtime: 220.0,
+  },
+  {
+    rating: 8.2,
+    runtime: 230.0,
+  },
+  {
+    rating: 7.9,
+    runtime: 240.0,
+  },
+  {
+    rating: 7.6,
+    runtime: 270.0,
+  },
+  {
+    rating: 7.3,
+    runtime: 280.0,
+  },
+  {
+    rating: 7.7,
+    runtime: 310.0,
+  },
+  {
+    rating: 8.2,
+    runtime: 320.0,
+  },
+];
