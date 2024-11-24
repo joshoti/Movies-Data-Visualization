e53669176d9bcd87d2c62d6556662229d7c38b68
joshoti <joshoti539@gmail.com>
2024-11-23 21:45:09 -0800

    Added sample data for genre composite chart

---
+++ src/app/components/data/GenreCompositeData.ts
@@ -0,0 +1,75 @@
+type genreData = {
+  main_genre: string;
+  runtime: number;
+  total_gross: number;
+};
+
+export type GenreCompositeT = Array<genreData>;
+
+export const genreCompositeData: GenreCompositeT = [
+  {
+    main_genre: "Action",
+    runtime: 183135.0,
+    total_gross: 101934.14,
+  },
+  {
+    main_genre: "Animation",
+    runtime: 30076.0,
+    total_gross: 30775.94,
+  },
+  {
+    main_genre: "Biography",
+    runtime: 43968.0,
+    total_gross: 10614.26,
+  },
+  {
+    main_genre: "Adventure",
+    runtime: 33862.0,
+    total_gross: 15685.52,
+  },
+  {
+    main_genre: "Western",
+    runtime: 573.0,
+    total_gross: 64.66,
+  },
+  {
+    main_genre: "Drama",
+    runtime: 120875.0,
+    total_gross: 25684.74,
+  },
+  {
+    main_genre: "Crime",
+    runtime: 51675.0,
+    total_gross: 9936.4,
+  },
+  {
+    main_genre: "Comedy",
+    runtime: 141122.0,
+    total_gross: 50404.58,
+  },
+  {
+    main_genre: "Horror",
+    runtime: 14052.0,
+    total_gross: 4877.3,
+  },
+  {
+    main_genre: "Mystery",
+    runtime: 3009.0,
+    total_gross: 683.51,
+  },
+  {
+    main_genre: "Film-Noir",
+    runtime: 314.0,
+    total_gross: 11.95,
+  },
+  {
+    main_genre: "Fantasy",
+    runtime: 1328.0,
+    total_gross: 533.02,
+  },
+  {
+    main_genre: "Musical",
+    runtime: 216.0,
+    total_gross: 0.0,
+  },
+];
