316171876efdd2169e09adfafa9bb892c354244c
joshoti <joshoti539@gmail.com>
2024-11-23 21:47:33 -0800

    Setup offline data for genreComposite component

--- src/app/components/AnalysisCharts/GenreComposite.tsx
+++ src/app/components/AnalysisCharts/GenreComposite.tsx
@@ -2,40 +2,27 @@ import { Flex, Title, Text } from "@mantine/core";
 import { CompositeChart } from "@mantine/charts";
 import { marginTop, chartHeight } from "../Analysis/Analysis";
 import classes from "../Analysis/Analysis.module.css";
+import {
+  genreCompositeData,
+  GenreCompositeT,
+} from "../data/GenreCompositeData";
+import { api } from "../../api/axios-api";
 
 export default function GenreCompositeChart() {
-  const data = [
-    {
-      genre: "Action",
-      gross: 1090,
-      runtime: 1200,
-    },
-    {
-      genre: "Horror",
-      gross: 1600,
-      runtime: 630,
-    },
-    {
-      genre: "Sci-Fi",
-      gross: 700,
-      runtime: 700,
-    },
-    {
-      genre: "Thriller",
-      gross: 1600,
-      runtime: 802,
-    },
-    {
-      genre: "Drama",
-      gross: 2000,
-      runtime: 910,
-    },
-  ];
-  const formattedData = data.map((item) => ({
-    genre: item.genre,
-    "Total Gross": item.gross,
-    "Runtime (mins)": item.runtime,
-  }));
+  // default initialization
+  let genreData: GenreCompositeT = [];
+
+  api
+    .get("/analysis/sample-4")
+    .then(({ data }) => {
+      console.log("SAMPLE" + data);
+      genreData = data;
+    })
+    .catch((error) => {});
+
+  if (genreData.length === 0) {
+    genreData = genreCompositeData;
+  }
 
   return (
     <Flex direction={"column"}>
@@ -48,22 +35,31 @@ export default function GenreCompositeChart() {
         Gross By Runtime vs Genre
       </Title>
       <Text mb={10} className={classes.regularText}>
-        Use Case: Examine the relationship between Total_Gross and Runtime,
+        Use Case: Examine the relationship between Total Gross and Runtime,
         grouped by Genre.
       </Text>
       <CompositeChart
         h={chartHeight - 150}
-        data={formattedData}
-        dataKey={"genre"}
+        pl={10}
+        pt={10}
+        data={genreData}
+        dataKey={"main_genre"}
+        xAxisLabel="Genres"
         withBarValueLabel
         withPointLabels
         series={[
           {
-            name: "Runtime (mins)",
+            name: "runtime",
+            label: "Runtime (Mins)",
             color: "rgba(18, 120, 255, 0.2)",
             type: "bar",
           },
-          { name: "Total Gross", color: "red.8", type: "line" },
+          {
+            name: "total_gross",
+            label: "Total Gross",
+            color: "red.8",
+            type: "line",
+          },
           { name: "Rating", color: "yellow.8", type: "area" },
         ]}
         curveType="natural"
