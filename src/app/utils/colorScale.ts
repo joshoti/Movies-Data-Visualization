645ad60d3d5ff79274f5d24667a0e2e7db01a04a
joshoti <joshoti539@gmail.com>
2024-11-25 17:12:03 -0800

    Finished colorScale function for heatmap

--- src/app/utils/colorScale.ts
+++ src/app/utils/colorScale.ts
@@ -1,18 +1,40 @@
-const colorScale: Record<number, string> = {
-  0: "rgb(255,0,0)",
-  10: "rgb(255,128,0)",
-  20: "rgb(255,255,0)",
-  30: "rgb(128,255,0)",
-  40: "rgb(0,255,0)",
-  50: "rgb(0,255,128)",
-  60: "rgb(0,255,255)",
-  70: "rgb(0,128,255)",
-  80: "rgb(0,0,255)",
-  90: "rgb(128,0,255)",
-  100: "rgb(255,0,255)",
-};
+import * as d3 from "d3-scale";
+import * as d3c from "d3-scale-chromatic";
 
 export function getColorScale(min: number, max: number, value: number) {
+  return colorScaleUsingRecord(min, max, value);
+}
+
+const colorScale: Record<number, string> = {
+  0: "rgb(238, 245, 252)",
+  10: "rgb(224, 236, 248)",
+  20: "rgb(210, 227, 243)",
+  30: "rgb(172, 208, 231)",
+  40: "rgb(134, 189, 220)",
+  50: "rgb(89, 157, 203)",
+  60: "rgb(45, 124, 186)",
+  70: "rgb(33, 110, 176)",
+  80: "rgb(21, 96, 167)",
+  90: "rgb(14, 74, 139)",
+  100: "rgb(8, 51, 111)",
+};
+
+const colorScaleUsingRecord = (min: number, max: number, value: number) => {
   const percent = Math.round(((value - min) / (max - min)) * 10);
   return colorScale[percent * 10];
-}
+};
+
+/**
+ * The below are interpolation functions depending on which theme color preferred
+ * 1. red theme - interpolateReds
+ * 2. green theme - interpolateBuGn, interpolateGreens
+ * 3. blue theme - interpolateBlues, interpolatePuBu
+ * 4. purple theme - interpolateRdPu
+ */
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const colorScaleusingD3 = (min: number, max: number, value: number) => {
+  const colorFunction = d3
+    .scaleSequential(d3c.interpolateBlues)
+    .domain([min, max]);
+  return colorFunction(value);
+};
