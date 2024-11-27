7ed8f112bc926a2e84ba36ca2dd130660b3209c6
joshoti <joshoti539@gmail.com>
2024-11-26 19:04:41 -0800

    Properly formatted chat interface

--- src/app/pages/Agent.tsx
+++ src/app/pages/Agent.tsx
@@ -2,19 +2,31 @@ import { ProbeHeader } from "../components/LLMAgent/ProbeHeader";
 import { PredictHeader } from "../components/LLMAgent/PredictHeader";
 import { RedirectToAnalysis } from "./RedirectToHome";
 import { ChatSpace } from "../components/LLMAgent/ChatSpace";
+import { InputField } from "../components/LLMAgent/InputField";
+import { Divider } from "@mantine/core";
+import { Flex } from "@mantine/core";
 
 export function LLMAgentTemplate({ application }: any) {
   return (
-    <>
-      {application === "probe" ? (
-        <ProbeHeader />
-      ) : application === "predict" ? (
-        <PredictHeader />
-      ) : (
-        <RedirectToAnalysis />
-      )}
-      <ChatSpace />
-      {/* < MessageBox application={application} /> */}
-    </>
+    <Flex direction={"column"} h={"100%"}>
+      <div
+        style={{
+          flex: 1,
+          padding: "20px",
+          overflowY: "auto",
+        }}
+      >
+        {application === "probe" ? (
+          <ProbeHeader />
+        ) : application === "predict" ? (
+          <PredictHeader />
+        ) : (
+          <RedirectToAnalysis />
+        )}
+        <Divider />
+        <ChatSpace />
+      </div>
+      <InputField application={application} />
+    </Flex>
   );
 }
