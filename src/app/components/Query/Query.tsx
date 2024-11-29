63e9ccbe92f3685cc1aa5f7a7ae66ba3bab5c12d
joshoti <joshoti539@gmail.com>
2024-11-29 01:30:46 -0800

    Removed static queryTable definition

--- src/app/components/Query/Query.tsx
+++ src/app/components/Query/Query.tsx
@@ -1,25 +1,11 @@
-import { Text, Flex, Title, Table, Box } from "@mantine/core";
+import { Text, Flex, Title, Box } from "@mantine/core";
 import classes from "./Query.module.css";
-import { table } from "./Table";
 import { marginTop } from "../Analysis/Analysis";
 import QueryBox from "./QueryBox";
 import { DynamicTable } from "./DynamicTable";
 
 export function Query() {
-  const tableRows = table.map((row) => (
-    <Table.Tr key={row.Movie_Title}>
-      <Table.Td>{row.Movie_Title}</Table.Td>
-      <Table.Td>{row.Year}</Table.Td>
-      <Table.Td>{row.Director}</Table.Td>
-      <Table.Td>{row.Actors}</Table.Td>
-      <Table.Td>{row.Rating}</Table.Td>
-      <Table.Td>{row["Runtime(Mins)"]}</Table.Td>
-      <Table.Td>{row.Censor}</Table.Td>
-      <Table.Td>{row.Total_Gross}</Table.Td>
-      <Table.Td>{row.main_genre}</Table.Td>
-      <Table.Td>{row.side_genre}</Table.Td>
-    </Table.Tr>
-  ));
+
 
   return (
     <Flex>
@@ -38,23 +24,6 @@ export function Query() {
         <QueryBox />
         <Box className={classes.table}>
           <DynamicTable />
-          {/* <Table withColumnBorders verticalSpacing="sm" horizontalSpacing="md">
-            <Table.Thead>
-              <Table.Tr className={classes.tableHeader}>
-                <Table.Th>Movie Title</Table.Th>
-                <Table.Th>Year</Table.Th>
-                <Table.Th>Director</Table.Th>
-                <Table.Th>Actors</Table.Th>
-                <Table.Th>Rating</Table.Th>
-                <Table.Th>Runtime (Mins)</Table.Th>
-                <Table.Th>Censor</Table.Th>
-                <Table.Th>Total Gross</Table.Th>
-                <Table.Th>Main Genre</Table.Th>
-                <Table.Th>Side Genre</Table.Th>
-              </Table.Tr>
-            </Table.Thead>
-            <Table.Tbody className={classes.tableBody}>{tableRows}</Table.Tbody>
-          </Table> */}
         </Box>
       </Flex>
     </Flex>
