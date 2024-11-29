f7d840dc9985bcb36c630543e3ad964d75ac884c
joshoti <joshoti539@gmail.com>
2024-11-28 23:11:08 -0800

    Using tableProvider

--- src/app/pages/Query.tsx
+++ src/app/pages/Query.tsx
@@ -1,9 +1,10 @@
 import { Query } from "../components/Query/Query";
+import { TableProvider } from "../hooks/QueryTableProvider";
 
 export function QueryTable() {
   return (
-    <>
+    <TableProvider>
       <Query />
-    </>
+    </TableProvider>
   );
 }
